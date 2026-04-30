c523fd8c49a76f2a2c17b5b53239764f2ba7eaea
purpeliz <purple.lizard0102@gmail.com>
2026-05-01 01:18:21 +0300

    chore(code): add Round and Team variables

--- code/GameLoop/GameManager.cs
+++ code/GameLoop/GameManager.cs
@@ -1,7 +1,16 @@
+using HideAndSeek.GameLoop.Rules;
+
 namespace HideAndSeek.GameLoop;
 
 public sealed class GameManager : Component
 {
+	private Round Round = new();
+	private Team[] Teams = new Team[2]
+	{
+		new Team("Seekers", "Red"),
+		new Team("Hiders", "Blue")
+	};
+
 	protected override void OnUpdate()
 	{
 
