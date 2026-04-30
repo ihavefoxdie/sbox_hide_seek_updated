28896b8670fdc7d129c0e16f49fdd5f41b3589b8
purpeliz <purple.lizard0102@gmail.com>
2026-05-01 01:17:14 +0300

    docs(code): add documentation to LevelLoader.ChangeScene

--- code/GameLoop/LevelLoader.cs
+++ code/GameLoop/LevelLoader.cs
@@ -1,10 +1,4 @@
-﻿
-
-using Sandbox.Menu;
-using Sandbox.Utils.Interfaces;
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Sandbox.Utils.Interfaces;
 using System.Threading.Tasks;
 
 namespace HideAndSeek.GameLoop;
@@ -20,11 +14,19 @@ public class LevelLoader
 		Game.ActiveScene.Load(options);
 	}
 
+	/// <summary>
+	/// Change the current scene to a selected one.
+	/// </summary>
+	/// <param name="scenePath">A new scene to load.</param>
+	/// <param name="isAdditive">Loads as additive.</param>
+	/// <param name="scene">Current scene.</param>
+	/// <returns></returns>
 	public static async Task ChangeScene( string scenePath, bool isAdditive, Scene scene )
 	{
 		SceneLoadOptions options = new();
 		options.SetScene( scenePath );
 		options.IsAdditive = isAdditive;
+
 		List<Task> tasks = [];
 		scene.RunEvent( delegate ( IBeforeSceneChangeListener x )
 		{
