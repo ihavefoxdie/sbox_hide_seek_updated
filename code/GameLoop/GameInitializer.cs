a18bb2abd0ff9457ea992cb638c4d31627b9e4af
purpeliz <purple.lizard0102@gmail.com>
2026-05-04 11:47:01 +0300

    refactor: code cleanup and docs - add docs to the level editor - change method name from ChangeScene to TransitionToSceneAsync - add fallbacks in case things go wrong

--- code/GameLoop/GameInitializer.cs
+++ code/GameLoop/GameInitializer.cs
@@ -1,100 +1,79 @@
 ﻿using HideAndSeek.GameLoop.Rules;
 using HideAndSeek.UI;
 using HideAndSeek.UI.MainMenu;
+using System;
 using System.Threading.Tasks;
-
-
 
 namespace HideAndSeek.GameLoop;
 
 public class GameInitializer : GameObjectSystem
 {
-#if DEBUG
-	private static bool s_bDoDebugOnlyCode = true;
-#endif
-
-	//private bool _disposed = false;
-
+	#region Variables
 	private GameLoadingScreen _loadingScreen;
+	private readonly string _gameScenePath = "scenes/GameScene.scene";
+	private readonly string _mainMenuScenePath = "scenes/MainMenu.scene";
+	#endregion
+
+
+	#region Properties
 	public UserGameSettings Settings { get; private set; }
 	[Property][Sync] public string MapIdent { get; private set; }
+	#endregion
+
+
 
 	public GameInitializer( Scene scene ) : base( scene )
 	{
 		Listen( Stage.SceneLoaded, 11, Initialize, "GameInitialization" );
 	}
 
-	/*~GameInitializer()
+
+
+	private async void Initialize()
 	{
-		Log.Info( $"{this.GetType().ToString()} is disposed!" );
-		Dispose();
-	}
-
-	public override void Dispose()
-	{
-		if ( _disposed )
+		try
 		{
-			return;
-		}
-
-		_disposed = true;
-		GC.SuppressFinalize( this );
-
-		base.Dispose();
-	}*/
-
-	async void Initialize()
-	{
-		if ( Scene.GetAll<MainMenuUI>().Any() || Scene.GetAll<GameManager>().Any() || Scene.IsEditor )
-		{
-#if DEBUG
-			Log.Warning( $"A special scene. Skipping {this}" );
-#endif
-			return;
-		}
-
-		GameObject ganeManager = Scene.CreateObject();
-		ganeManager.Name = "GameLoop";
-		ganeManager.AddComponent<GameManager>();
-
-		_loadingScreen = Scene.GetAll<GameLoadingScreen>().FirstOrDefault();
-		Settings = new UserGameSettings();
-		Settings.LoadSettings();
-
-#if DEBUG
-		Log.Info( Settings.MapName );
-#endif
-
-		MapIdent = Settings.MapName;
-
-
-		Task<Package> task = Package.Fetch( Settings.MapName, true );
-		task.Wait();
-		Package info = task.Result;
-		if ( info != null )
-		{
-			if ( info.TypeName == "map" )
+			if ( Scene.GetAll<MainMenuUI>().Any() || Scene.GetAll<GameManager>().Any() || Scene.IsEditor )
 			{
-				MapIdent = info.FullIdent;
+				return;
 			}
-		}
-		else
-		{
-			MapIdent = "facepunch.flatgrass";
-		}
 
-		_loadingScreen?.LoadingElement = MapIdent;
+			GameObject ganeManager = Scene.CreateObject();
+			ganeManager.Name = "GameLoop";
+			ganeManager.AddComponent<GameManager>();
 
-		await Task.Delay( 5000 );
-		GameObject map = Scene.CreateObject();
-		map.Name = "Map";
-		MapInstance instance = map.AddComponent<MapInstance>();
-		instance.MapName = MapIdent;
-		instance.OnMapLoaded += async () =>
+			Settings = new UserGameSettings();
+			Settings.LoadSettings();
+			MapIdent = Settings.MapName;
+
+			Package info = await Package.FetchAsync( Settings.MapName, true );
+			MapIdent = info?.TypeName == "map" ? info.FullIdent : "facepunch.flatgrass";
+			
+			_loadingScreen = Scene.GetAll<GameLoadingScreen>().FirstOrDefault();
+			_loadingScreen?.LoadingElement = MapIdent;
+
+			await Task.Delay( 5000 );
+
+			GameObject map = Scene.CreateObject();
+			map.Name = "Map";
+			MapInstance instance = map.AddComponent<MapInstance>();
+			instance.MapName = MapIdent;
+			instance.OnMapLoaded += async () =>
+			{
+				Log.Info( "Map has been loaded!" );
+				bool loaded = await LevelLoader.TransitionToSceneAsync( _gameScenePath, true, Scene );
+				if ( !loaded )
+				{
+					Log.Error( $"Failed to load {_gameScenePath}." );
+					LevelLoader.ChangeScene( _mainMenuScenePath, false );
+				}
+				_loadingScreen?.GameObject?.Destroy();
+			};
+		}
+		catch ( Exception ex )
 		{
-			Log.Info( "Map has been loaded!" );
-			await LevelLoader.ChangeScene( "scenes/GameScene.scene", true, Scene );
-			_loadingScreen?.GameObject?.Destroy();
-		};
+			Log.Error( $"Failed to initialize the game: {ex}" );
+			LevelLoader.ChangeScene( _mainMenuScenePath, false );
+		}
 	}
 }
--- code/GameLoop/LevelLoader.cs
+++ code/GameLoop/LevelLoader.cs
@@ -1,42 +1,81 @@
 ﻿using Sandbox.Utils.Interfaces;
+using System;
 using System.Threading.Tasks;
 
 namespace HideAndSeek.GameLoop;
 
-public class LevelLoader
+public static class LevelLoader
 {
-	public static void ChangeScene(string scenePath, bool isAdditive)
+	/// <summary>
+	/// Changes the active scene to the specified one.
+	/// </summary>
+	/// <param name="scenePath">The path of the scene to load.</param>
+	/// <param name="isAdditive">Loads as additive.</param>
+	/// <c>true</c> if the scene load was initiated successfully; otherwise, <c>false</c>.
+	public static bool ChangeScene(string scenePath, bool isAdditive)
 	{
-		SceneLoadOptions options = new();
-		options.SetScene(scenePath);
-		options.IsAdditive = isAdditive;
+		try
+		{
+			SceneLoadOptions options = new();
+			options.SetScene( scenePath );
+			options.IsAdditive = isAdditive;
+			Game.ChangeScene( options );
 
-		Game.ActiveScene.Load(options);
+			return true;
+		}
+		catch ( Exception ex )
+		{
+			Log.Error( ex, $"Failed to load the {scenePath} scene." );
+			return false;
+		}
 	}
 
 	/// <summary>
-	/// Change the current scene to a selected one.
+	/// Asynchronously changes the active scene to the specified one.
+	/// Before loading, it notifies all components implementing <see cref="IBeforeSceneChangeListener"/>
+	/// and awaits their completion.
 	/// </summary>
-	/// <param name="scenePath">A new scene to load.</param>
+	/// <param name="scenePath">The path of the scene to load.</param>
 	/// <param name="isAdditive">Loads as additive.</param>
-	/// <param name="scene">Current scene.</param>
-	/// <returns></returns>
-	public static async Task ChangeScene( string scenePath, bool isAdditive, Scene scene )
+	/// <param name="scene">The current scene used to resolve listeners.</param>
+	/// <returns>
+	/// <c>true</c> if the scene load was initiated successfully; otherwise, <c>false</c>.
+	/// </returns>
+	public static async Task<bool> TransitionToSceneAsync( string scenePath, bool isAdditive, Scene scene )
 	{
-		SceneLoadOptions options = new();
-		options.SetScene( scenePath );
-		options.IsAdditive = isAdditive;
-
-		List<Task> tasks = [];
-		scene.RunEvent( delegate ( IBeforeSceneChangeListener x )
+		try
 		{
-			tasks.Add(x.BeforeChange());
+			if ( string.IsNullOrEmpty( scenePath ) )
+			{
+				return false;
+			}
+			SceneLoadOptions options = new();
+			options.SetScene( scenePath );
+			options.IsAdditive = isAdditive;
+			options.ShowLoadingScreen = false;
+
+			await InvokeBeforeSceneChangeAsync(scene);
+
+			Game.ChangeScene( options );
+			return true;
+		}
+		catch ( Exception ex )
+		{
+			Log.Error( ex, $"Failed to load the {scenePath} scene." );
+			return false;
+		}
+	}
+
+	private static async Task InvokeBeforeSceneChangeAsync(Scene scene)
+	{
+		List<Task> tasks = [];
+		scene?.RunEvent( delegate ( IBeforeSceneChangeListener x )
+		{
+			tasks.Add( x.BeforeChange() );
 		} );
 		foreach ( var task in tasks )
 		{
 			await task;
 		}
-
-		Game.ActiveScene.Load( options );
 	}
 }
