12bef3e9cc90970f1f8eec53a7b641b2d0f430b7
ihavefoxdie <vladislav.nekrasov123@gmail.com>
2026-05-05 18:17:51 +0300

    feat: add RoundStateResolver to allow a more flexible control over state changes

--- code/GameLoop/States/RoundStateResolver.cs
+++ code/GameLoop/States/RoundStateResolver.cs
@@ -1,9 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using HideAndSeek.GameLoop.StateMachines;
+using HideAndSeek.GameLoop.States;
 
 namespace Sandbox.GameLoop.States;
 
 public class RoundStateResolver
 {
+	public static IRoundState ResolveState( IRoundState state, RoundStateMachine stateMachine )
+	{
+		switch ( state )
+		{
+			case PreparingRoundState:
+				return new ActiveRoundState( stateMachine );
+			case ActiveRoundState:
+				return new PostRoundState( stateMachine );
+			case PostRoundState:
+				return new PreparingRoundState( stateMachine );
+			case WaitingRoundState:
+				return new PreparingRoundState( stateMachine );
+			default:
+				return new PreparingRoundState( stateMachine );
+		}
+	}
 }
