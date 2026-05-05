b51d809e0d1d525df25cd2abbd5eef3ce9caf0c3
purpeliz <purple.lizard0102@gmail.com>
2026-05-05 15:07:28 +0300

    feat: add RoundStateMachine

---
+++ code/GameLoop/StateMachines/RoundStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using HideAndSeek.GameLoop.Rules;
+using HideAndSeek.GameLoop.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HideAndSeek.GameLoop.StateMachines
+{
+	public class RoundStateMachine : Component
+	{
+		public Round Round { get; private set; }
+		private IRoundState _currentState;
+		public event Action<IRoundState> OnStateChanged;
+
+		[Rpc.Broadcast]
+		public void ChangeState( IRoundState state )
+		{
+			_currentState = state;
+		}
+
+		protected void SomethingSomething()
+		{
+			if ( _currentState != null )
+			{
+				_currentState.Update();
+			}
+		}
+	}
+}
