a65236c2721c92ccb76ade42385c08da86b1b370
purpeliz <purple.lizard0102@gmail.com>
2026-05-10 12:18:58 +0300

    refactor: change the Round class - rearrange methods - call basic ctor on the parameterized one - fix docs - add missing docs

--- code/GameLoop/Rules/Round.cs
+++ code/GameLoop/Rules/Round.cs
@@ -16,6 +16,11 @@ public class Round
 	public int CurrentRoundCount { get; private set; } = 0;
 
 	/// <summary>
+	/// Is round in progress?
+	/// </summary>
+	public bool IsStarted { get; private set; } = false;
+
+	/// <summary>
 	/// Round length, in seconds.
 	/// </summary>
 	/// <remarks>Cannot be less than 10 seconds.</remarks>
@@ -28,70 +33,6 @@ public class Round
 			{
 				field = value;
 			}
-		}
-	}
-
-	/// <summary>
-	/// Gets the maximum number of rounds allowed.
-	/// </summary>
-	/// <remarks>Cannot be less than 1.</remarks>
-	public int RoundLimit
-	{
-		get;
-		private set
-		{
-			if ( value > 0 )
-			{
-				field = value;
-			}
-		}
-	}
-
-	/// <summary>
-	/// Is round in progress?
-	/// </summary>
-	public bool IsStarted { get; private set; } = false;
-
-	/// <summary>
-	/// Should the round end?
-	/// </summary>
-	public bool ShouldRoundEnd
-	{
-		get
-		{
-			if ( TimeSinceStart.Relative >= RoundLength + PrepTime && IsStarted )
-			{
-				return true;
-			}
-			return false;
-		}
-	}
-
-	public bool ShouldSeekersBeEnabled
-	{
-		get
-		{
-			if ( TimeSinceStart.Relative >= PrepTime && IsStarted )
-			{
-				return true;
-			}
-			return false;
-		}
-	}
-
-	public bool ShouldTheGameEnd
-	{
-		get
-		{
-			return CurrentRoundCount >= RoundLimit;
-		}
-	}
-
-	public bool HasPostRoundEnded
-	{
-		get
-		{
-			return TimeSinceStart.Relative >= RoundLength + PrepTime + TimeBeforeNextRound;
 		}
 	}
 
@@ -125,6 +66,76 @@ public class Round
 			}
 		}
 	}
+
+	/// <summary>
+	/// Gets the maximum number of rounds allowed.
+	/// </summary>
+	/// <remarks>Cannot be less than 1.</remarks>
+	public int RoundLimit
+	{
+		get;
+		private set
+		{
+			if ( value > 0 )
+			{
+				field = value;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Should the round end?
+	/// </summary>
+	public bool ShouldRoundEnd
+	{
+		get
+		{
+			if ( TimeSinceStart.Relative >= RoundLength + PrepTime && IsStarted )
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// Has the preparation time period passed, allowing seekers to be enabled?
+	/// </summary>
+	public bool ShouldSeekersBeEnabled
+	{
+		get
+		{
+			if ( TimeSinceStart.Relative >= PrepTime && IsStarted )
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// Gets a value indicating whether the game should end because the current round count has reached or exceeded the
+	/// round limit.
+	/// </summary>
+	/// <remarks>True when CurrentRoundCount is greater than or equal to RoundLimit.</remarks>
+	public bool ShouldTheGameEnd
+	{
+		get
+		{
+			return CurrentRoundCount >= RoundLimit;
+		}
+	}
+
+	/// <summary>
+	/// Has the post-round period ended, meaning that the next round can start?
+	/// </summary>
+	public bool HasPostRoundEnded
+	{
+		get
+		{
+			return TimeSinceStart.Relative >= RoundLength + PrepTime + TimeBeforeNextRound;
+		}
+	}
 	#endregion
 
 
@@ -146,7 +157,7 @@ public class Round
 		TimeBeforeNextRound = DefaultTimeBeforeNextRound;
 	}
 
-	public Round( int roundLength, int roundsLimit, int timeBeforeNextRound, int prepTime)
+	public Round( int roundLength, int roundsLimit, int timeBeforeNextRound, int prepTime) : this()
 	{
 		RoundLength = roundLength;
 		RoundLimit = roundsLimit;
@@ -156,11 +167,10 @@ public class Round
 
 	#region Methods
 	/// <summary>
-	/// Starts a round when not already started and when CanRoundStart is true.
+	/// Starts a round when not already started and when ShouldTheGameEnd is false.
 	/// </summary>
-	/// <remarks>On success, sets IsStarted to true, resets TimeSinceStart to 0, and increments CurrentRoundCount.
-	/// No state changes occur if the round is not started.</remarks>
-	/// <returns><c>true</c> if the round was started; otherwise <c>false</c> (for example, if already started or CanRoundStart is <c>false</c>).</returns>
+	/// <remarks>On success, sets IsStarted to true, resets TimeSinceStart to 0, and increments CurrentRoundCount.</remarks>
+	/// <returns><c>true</c> if the round was started; otherwise <c>false</c> (for example, if already started or ShouldTheGameEnd is <c>true</c>).</returns>
 	public bool StartRound()
 	{
 		if ( !IsStarted && !ShouldTheGameEnd )
